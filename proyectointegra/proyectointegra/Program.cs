3c282f360ac56b3a5b1aee05cd38197b610df0dd
mgiorgi32 <117841206+mgiorgi32@users.noreply.github.com>
2023-06-14 13:20:54 -0300

    Update Program.cs

--- proyectointegra/proyectointegra/Program.cs
+++ proyectointegra/proyectointegra/Program.cs
@@ -1,4 +1,4 @@
-﻿using proyectointegra;
+using proyectointegra;
 using System;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -9,83 +9,76 @@ namespace BancoSA{
 
         public static void Main()
         {
-            Console.WriteLine("Bienvenido a la pagina de prestamos, vamos a precisar unos datos para avanzar: ");
+            bool otro = true;
+            while (otro)
+            {
+                Console.WriteLine("Bienvenido a la pagina de prestamos, vamos a precisar unos datos para avanzar: ");
             Console.ReadLine();
-            persona persona = new persona(11111111, lucas, hernandez, 222222222, 232343);
+                var personas = new List<Persona>()
+                {
+                    new Persona(12345678, "Fernando", "Alonso", 1122222222, 023766666),
+                    new Persona(87654321, "Lance", "Stroll", 1111111111, 0237111111),
+                    new Persona(21436587, "Clint", "Barton", 1133333333, 0237222222),
+                    new Persona(13245768, "Tony", "Stark", 1144444444, 0237333333),
+                    new Persona(86754231, "Luke", "Skywalker", 1155555555, 0237444444),
+                    new Persona(18273645, "El pollo", "Vignolo", 1166666666, 0237555555),
+                    new Persona(54637281, "Lionel", "Messi", 1177777777, 0237777777),
+                    new Persona(45362718, "Seth", "Rollins", 1188888888, 0237888888),
+                    new Persona(76543210,"Taylor", "Swift", 1199999999,0237999999),
+                    new Persona(65432198, "Kick", "Butowski", 1100000000, 0237000000),
+                };
+            Persona persona = personas.Where(x => x.Dni == 12345678).First();            
             Console.WriteLine (persona.Dni);
-            Console.ReadLine();
             Console.WriteLine(persona.Nombre);
-            Console.ReadLine();
             Console.WriteLine(persona.Apellido);
-            Console.ReadLine();
             Console.WriteLine(persona.TelefonoMovil);
-            Console.ReadLine();
             Console.WriteLine(persona.TelefonoCasa);
-            Console.ReadLine();
             Console.WriteLine("Bienvenido denuevo {0}.", persona.Nombre);
-            Console.ReadLine();
             Console.WriteLine("Muchas gracias, sigamos con el proceso");
             Console.ReadLine();
             Console.Clear();
 
-            prestamo prestamo = new prestamo { numeroPrestamo = 1, FechaAutorizacion = DateTime.Now };
+            Prestamo prestamo = new Prestamo { numeroPrestamo = 1, FechaAutorizacion = DateTime.Now };
+                Console.WriteLine("Lo maximo que puedes pedir es: " + 250000 + " ");
             Console.WriteLine("¿De cuanto seria su prestamo?" );
             Console.ReadLine();
-            prestamo.ValorPrestamo.ToString(Console.ReadLine()); //preguntar 
+                prestamo.ValorPrestamo = decimal.Parse(Console.ReadLine());
             Console.WriteLine(prestamo.ValorPrestamo);
-            Console.WriteLine("Este prestamo puede ser pagado en 3, 6, 12 0 18 cuotas, el plan de 12 y 18 cuotas tiene un interes y tambien puede ser pagado en un pago aunque no es recomendable, en cuantas cuotas quiere sacar este prestamo: " );
+            Console.WriteLine("Este prestamo puede ser pagado en 3, 6, 12 0 18 cuotas, el plan de 12 y 18 cuotas tiene un interes y tambien puede ser pagado en un pago aunque no es recomendable,  en cuantas cuotas quiere sacar este prestamo: " );
             int cuota = Convert.ToInt32(Console.ReadLine());
-            if(cuota==3)
-            {
-               int valor = (int)(prestamo.ValorPrestamo /= 3);
-                Console.WriteLine($"Las cuotas serian 3 de: {valor}");
-                prestamo.FechaPagos = new List<DateTime> { prestamo.FechaTentativa.AddMonths(1), prestamo.FechaTentativa.AddMonths(2), prestamo.FechaTentativa.AddMonths(3) };
-                Console.WriteLine("Sus fechas de pago serian: " + prestamo.FechaPagos.Count);
-            }else if (cuota == 6)
-            {
-                int valor = (int)(prestamo.ValorPrestamo /= 6);
-                Console.WriteLine($"Las cuotas serian 6 de: {valor}");
-                prestamo.FechaPagos = new List<DateTime> { prestamo.FechaTentativa.AddMonths(1), prestamo.FechaTentativa.AddMonths(2), prestamo.FechaTentativa.AddMonths(3), prestamo.FechaTentativa.AddMonths(4), prestamo.FechaTentativa.AddMonths(5), prestamo.FechaTentativa.AddMonths(6) };
-                Console.WriteLine("Sus fechas de pago serian: " + prestamo.FechaPagos.Count);
-            }else if (cuota == 12)
-            {
-                int valor = (int)((prestamo.ValorPrestamo*2)/12);
-                Console.WriteLine($"Las cuotas serian 12 de: {valor}");
-                prestamo.FechaPagos = new List<DateTime> { prestamo.FechaTentativa.AddMonths(1), prestamo.FechaTentativa.AddMonths(2), prestamo.FechaTentativa.AddMonths(3), prestamo.FechaTentativa.AddMonths(4), prestamo.FechaTentativa.AddMonths(5), prestamo.FechaTentativa.AddMonths(6), prestamo.FechaTentativa.AddMonths(7), prestamo.FechaTentativa.AddMonths(8), prestamo.FechaTentativa.AddMonths(9), prestamo.FechaTentativa.AddMonths(10), prestamo.FechaTentativa.AddMonths(11), prestamo.FechaTentativa.AddMonths(12) };
-                Console.WriteLine("Sus fechas de pago serian: " + prestamo.FechaPagos.Count);
-            }else if (cuota == 18)
-            {
-                int valor = (int)((prestamo.ValorPrestamo * 3) / 18);
-                Console.WriteLine($"Las cuotas serian 18 de: {valor}");
-                prestamo.FechaPagos = new List<DateTime> { prestamo.FechaTentativa.AddMonths(1), prestamo.FechaTentativa.AddMonths(2), prestamo.FechaTentativa.AddMonths(3), prestamo.FechaTentativa.AddMonths(4), prestamo.FechaTentativa.AddMonths(5), prestamo.FechaTentativa.AddMonths(6), prestamo.FechaTentativa.AddMonths(7), prestamo.FechaTentativa.AddMonths(8), prestamo.FechaTentativa.AddMonths(9), prestamo.FechaTentativa.AddMonths(10), prestamo.FechaTentativa.AddMonths(11), prestamo.FechaTentativa.AddMonths(12), prestamo.FechaTentativa.AddMonths(13), prestamo.FechaTentativa.AddMonths(14), prestamo.FechaTentativa.AddMonths(15), prestamo.FechaTentativa.AddMonths(16), prestamo.FechaTentativa.AddMonths(17), prestamo.FechaTentativa.AddMonths(18) };
-                Console.WriteLine("Sus fechas de pago serian: " + prestamo.FechaPagos.Count);
-            }else if(cuota == 1)
-            {
-                int valor = (int)(prestamo.ValorPrestamo);
-                Console.WriteLine($"El pago se haria en 1 pago de: {valor}");
-                Console.WriteLine("La fecha de pago seria el dia: {0}", prestamo.FechaTentativa.AddMonths(1));
-            }
-            else
-            {
+                var cuotaMaxima = 18;
+
+                if (cuota % 3 != 0 || cuota > cuotaMaxima || cuota < 3)
+                {
                 Console.WriteLine("Estas cuotas no estan disponibles intentelo denuevo mas tarde.");
-            }
-            Console.ReadLine();
-            Console.WriteLine("La fecha de autorizacion de uso seria: " + prestamo.FechaAutorizacion.ToString("dd/MM/yyyy") + " ");
-            Console.ReadLine();
-            Console.WriteLine("La fecha tentativa de ingreso seria: " + prestamo.FechaTentativa.ToString("dd/MM/yyyy") + " ");
-            Console.ReadLine();
-            Console.WriteLine("La fecha maxima de autorización para este prestamo seria: " + prestamo.FechaMax().ToString("dd/MM/yyyy") + " ");
-            prestamo.MontoMax(250000);
-            Console.WriteLine("Lo maximo que puedes pedir es: " + prestamo.MontoMax(3000 - (int)prestamo.ValorPrestamo) + " ");
-            Console.ReadLine();
-            bool otro = true;
-            while (true)
-            {
+                }
+                else
+                {
+                    int valor = (int)(prestamo.ValorPrestamo /= cuota);
+                    Console.WriteLine($"las cuotas serian {cuota} con un valor cada una de {valor}");
+                    prestamo.FechaPagos = new List<DateTime>();
+                    for( var i = 1; i<= cuota; i++ )
+                    {
+                        prestamo.FechaPagos.Add( prestamo.FechaTentativa.AddMonths(i) );
+                    }
+                    var mensajeFechas = "sus fechas de pago son: ";
+                    var separador = " ";
+                    for(var i=0; i<=prestamo.FechaPagos.Count; i++ )
+                    {
+                        mensajeFechas += $"{separador} {prestamo.FechaPagos[i]}";
+                        separador = ",";
+                    }
+                    Console.WriteLine(mensajeFechas );
+                    Console.WriteLine("La fecha de autorizacion de uso seria: " + prestamo.FechaAutorizacion.ToString("dd/MM/yyyy") + " ");
+                    Console.WriteLine("La fecha tentativa de ingreso seria: " + prestamo.FechaTentativa.ToString("dd/MM/yyyy") + " ");
+                    Console.WriteLine("La fecha maxima de autorización para este prestamo seria: " + prestamo.FechaMax().ToString("dd/MM/yyyy") + " ");
+                    Console.ReadLine();
+                }
                 Console.WriteLine("¿Desea pedir otro prestamo?");
                 string RTA = Console.ReadLine();
                 if( RTA == "si")
                 {
-                    otro = true;
+                    continue;
                 }else if ( RTA == "no")
                 {
                     otro = false;
